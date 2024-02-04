be6e6328c280986272bb1e2f809080c99c76b3a0
Titus T <dxtitus@gmail.com>
2024-02-04 20:43:26 +0000

    interval tree

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/intdxdt/intervaltree
+
+go 1.21
+
+require (
+	github.com/rdleal/intervalst v1.2.2 // indirect
+	golang.org/x/exp v0.0.0-20240119083558-1b970713d09a // indirect
+)
---
+++ interval.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/rdleal/intervalst/interval"
+	"golang.org/x/exp/constraints"
+)
+
+type CmpFn[I constraints.Ordered] func(x, y I) int
+
+type IntervalTree[I constraints.Ordered, V any] struct {
+	tree *interval.MultiValueSearchTree[V, I]
+}
+
+func NewIntervalTree[I constraints.Ordered, V any](cmp CmpFn[I]) *IntervalTree[I, V] {
+	return &IntervalTree[I, V]{
+		tree: interval.NewMultiValueSearchTreeWithOptions[V, I](
+			interval.CmpFunc[I](cmp), interval.TreeWithIntervalPoint(),
+		),
+	}
+}
+
+func (it *IntervalTree[I, V]) Add(i, j I, value V) *IntervalTree[I, V] {
+	if i > j {
+		i, j = j, i
+	}
+	_ = it.tree.Insert(i, j, value)
+	return it
+}
+
+func (it *IntervalTree[I, V]) At(i I) []V {
+	return it.Intersect(i, i)
+}
+
+func (it *IntervalTree[I, V]) Intersect(i, j I) []V {
+	values, _ := it.tree.AllIntersections(i, j)
+	return values
+}
